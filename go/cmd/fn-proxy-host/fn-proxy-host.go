48a7b91cca9782ccc89ff4d3e0b8c288df877c94
Andreas Fuchs <asf@boinkor.net>
2014-01-12 14:46:35 -0800

    Allow the host-bound proxy to translate file names
    The fn-proxy-host binary now takes a list of args of the form
    host-dir-prefix:guest-dir-prefix, which causes it to translate every
    file name in both directions.

--- go/cmd/fn-proxy-host/fn-proxy-host.go
+++ go/cmd/fn-proxy-host/fn-proxy-host.go
@@ -1,29 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"github.com/burke/zeus/go/filemonitor"
-	"io"
 	"net"
 	"os"
+	"strings"
+	"io"
 )
 
+var hostToGuest map[string]string = make(map[string]string)
+var guestToHost map[string]string = make(map[string]string)
+
+var verbose bool
+
 func main() {
-	if len(os.Args) != 2 {
-		println("USAGE: " + os.Args[0] + " listen-port")
+	verbose = true
+	if len(os.Args) < 2 {
+		println("USAGE: " + os.Args[0] + " listen-port [hostDir:guestDir] ...")
 		os.Exit(1)
 	}
+	port := os.Args[1]
 
-	ln, err := net.Listen("tcp", "127.0.0.1:"+os.Args[1])
+	dirMappings := os.Args[2:]
+	for _, mapping := range dirMappings {
+		dirs := strings.Split(mapping, ":")
+		if len(dirs) != 2 {
+			panic("Encountered malformed dir mapping spec " + mapping)
+		}
+		hostToGuest[dirs[0]] = dirs[1]
+		guestToHost[dirs[1]] = dirs[0]
+	}
+
+	ln, err := net.Listen("tcp", "127.0.0.1:"+port)
 	if err != nil {
-		println("Couldn't start listening on " + os.Args[1] + ":" + err.Error())
-		os.Exit(1)
+		panic("Couldn't start listening on " + os.Args[1] + ":" + err.Error())
 	}
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			println("Error accepting a connection: " + err.Error())
-			os.Exit(1)
+			panic("Error accepting a connection: " + err.Error())
 		}
 		go handleConnection(conn)
 	}
@@ -32,16 +49,44 @@ func main() {
 
 func handleConnection(conn net.Conn) {
 	cmd, watcherIn, watcherOut, _, err := filemonitor.StartWrapperProgram()
+	if err != nil {
+		panic("Couldn't start the host-side filemonitor wrapper: " + err.Error())
+	}
 	defer func() {
 		cmd.Process.Kill()
 		cmd.Wait()
 	}()
 
-	if err != nil {
-		println("Couldn't start the host-side filemonitor wrapper: " + err.Error())
-		os.Exit(1)
+	readErrors := make(chan error)
+	go proxyFiles(readErrors, bufio.NewWriter(conn), bufio.NewReader(watcherOut), hostToGuest, true)
+	go proxyFiles(readErrors, bufio.NewWriter(watcherIn), bufio.NewReader(conn), guestToHost, false)
+	select {
+	case err = <-readErrors:
+		if err != io.EOF {
+			println("Error handling the connection: " + err.Error())
+		}
 	}
+}
 
-	go io.Copy(conn, watcherOut)
-	io.Copy(watcherIn, conn)
+func proxyFiles(readErrors chan error, out *bufio.Writer, in *bufio.Reader, translation map[string]string, eofIsFatal bool) {
+	for {
+		originalFile, err := in.ReadString('\n')
+		if err != nil {
+			readErrors <- err
+			return
+		}
+		file := originalFile
+
+		// TODO: I'm sure this is plenty inefficient:
+		for dir, replacement := range translation {
+			if strings.HasPrefix(file, dir) {
+				file = replacement + file[len(dir):]
+			}
+		}
+		if verbose {
+			println("Got a file notification: " + originalFile + " but " + file)
+		}
+		out.WriteString(file)
+		out.Flush()
+	}
 }
